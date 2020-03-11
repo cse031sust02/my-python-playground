f63a173840563daafc097d87ce9b3b4d1ac1dd7a
Talha Ibne Imam <cse031sust02.freelancer@gmail.com>
2020-03-11 17:19:06 +0600

    some typo fixes

--- basic/variable-scope.py
+++ basic/variable-scope.py
@@ -6,7 +6,7 @@
 # Basic Example
 # In the example below, When we are in inner_function(), c is local to us, b is nonlocal and a is global.
 # We can read as well as assign new values to c but can only read b and a from inner_function().
-# src: https: // www.programiz.com/python-programming/namespace  # example
+# src: https://www.programiz.com/python-programming/namespace
 
 def outer_function():
     b = 20
@@ -18,23 +18,22 @@ def outer_function():
 a = 10
 
 # More Details
-# src: https: // www.programiz.com/python-programming/keyword-list
+# src: https://www.programiz.com/python-programming/keyword-list
 
 # global: global is used to declare that a variable inside the function is global (outside the function).
 # If we need to read the value of a global variable, it is not necessary to define it as global.
 # If we need to modify the value of a global variable inside a function, then we must declare it with global.
 # Otherwise a local variable with that name is created.
 
-# nonlocal: very much similar to the global keyword. nonlocal is used to declare that a variable inside a
-# nested function is not local to it.
-# If we need to modify the value of a non-local variable inside a nested function, then we must declare it with nonlocal.
+# nonlocal: very much similar to the global keyword. nonlocal is used to declare that a variable inside a nested function is not local 
+# to it. If we need to modify the value of a non-local variable inside a nested function, then we must declare it with nonlocal. 
 # Otherwise a local variable with that name is created inside the nested function.
 
 x = 'Global X'
 
 
 def outer_func():
-    # to modily global x, uncomment next line
+    # to modify global x, uncomment next line
     # global x
     x = 'Outer X'
 
