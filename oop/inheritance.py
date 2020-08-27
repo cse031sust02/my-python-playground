6b7474ac88b5d070e770f9d6ee28b266fdf938ea
cse031sust02 <cse031sust02.freelancer@gmail.com>
2020-08-27 15:31:20 +0600

    modify inheritance

--- oop/inheritance.py
+++ oop/inheritance.py
@@ -1,10 +1,14 @@
 class Animal:
+    """Baseclass for all animals"""
 
     def __init__(self, name, age):
         self.name = name
         self.age = age
 
     def speak(self, sound):
+        """An animal will speak in it's own language.
+        :param sound: sound of that animal
+        """
         print('{} is speaking. ({})'.format(self.name, sound))
 
 
@@ -28,17 +32,25 @@ class Bird(Animal):
 
     def __init__(self, name, age, can_fly=True):
         super().__init__(name, age)
+        # Alternative way :
+        # Animal.__init__(self, name, age)
         self.can_fly = can_fly
 
     def speak(self):
         super().speak('pee pee')
 
     def fly(self):
-        if self.can_fly == True:
+        """There are some birds which can not fly. It will check the value
+        of 'can_fly' variable to determince if it can or can not fly
+        """
+        if self.can_fly:
             print('.....{} is flying....'.format(self.name))
         else:
             print('{} can not fly!'.format(self.name))
 
+# Bird is a subclass of Animal
+print(issubclass(Bird, Animal))
+print(issubclass(Bird, Cat))
 
 bird_1 = Bird('Demo Bird 1', 1)
 bird_1.speak()
