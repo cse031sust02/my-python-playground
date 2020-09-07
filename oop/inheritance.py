df467846380a78699a63723206d290bfa38bbe18
cse031sust02 <cse031sust02.freelancer@gmail.com>
2020-09-07 11:39:36 +0600

    add description and multiple inheritance example

--- oop/inheritance.py
+++ oop/inheritance.py
@@ -1,3 +1,33 @@
+# What is Inheritance :
+# ===========================
+#
+# Inheritance is a powerful feature in object oriented programming.
+#
+# If we think of inheritance in terms of biology, we can think of a
+# child inheriting certain characteristic from their parent. Similarly,
+# in OOP languages, child classes (or subclasses) inherit methods and
+# data attributes from parent classes (or base classes).
+#
+# Python supports multiple inheritance (Unlike Java, PHP, C# etc) and
+# multilevel inheritance.
+#
+#
+# Advantages :
+# ===================
+#
+# One of the key benefits of inheritance is to minimize the amount of
+# duplicate code by resuing code. Inheritance also helps us to better
+# organize the code in smaller and simpler units. It also represents
+# real-world relationships well.
+#
+#
+# More details/discussions :
+# =============================
+#
+# - https://www.geeksforgeeks.org/inheritance-in-python/
+# - https://www.digitalocean.com/community/tutorials/understanding-class-inheritance-in-python-3
+
+
 class Animal:
     """Baseclass for all animals"""
 
@@ -28,6 +58,12 @@ print(isinstance(cat_1, Animal))
 # help(cat_1)
 
 
+# The super() Function :
+# =========================
+# We use the super() function to get a parent method into a child
+# method to make use of it.
+
+
 class Bird(Animal):
 
     def __init__(self, name, age, can_fly=True):
@@ -48,6 +84,7 @@ class Bird(Animal):
         else:
             print('{} can not fly!'.format(self.name))
 
+
 # Bird is a subclass of Animal
 print(issubclass(Bird, Animal))
 print(issubclass(Bird, Cat))
@@ -59,3 +96,32 @@ bird_1.fly()
 penguin = Bird('Penguin', 1, False)
 penguin.speak()
 penguin.fly()
+
+
+# Multiple Inheritance :
+# =========================
+# When a child class inherits from multiple parent classes, it is
+# called multiple inheritance.
+
+class Car:
+
+    def run(self):
+        print("I am running on road")
+
+
+class Aircraft:
+
+    def fly(self):
+        print("I am flying on the sky")
+
+
+class FlyingCar(Car, Aircraft):
+
+    def start(self):
+        print("I am a Flying Car!")
+        self.run()
+        self.fly()
+
+
+my_flying_car = FlyingCar()
+my_flying_car.start()
