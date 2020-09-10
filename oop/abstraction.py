cc9175ae7cd2783488222770cf193207f31a1883
cse031sust02 <cse031sust02.freelancer@gmail.com>
2020-09-10 10:38:41 +0600

    restructure files and folders

---
+++ oop/abstraction.py
@@ -0,0 +1,18 @@
+# What is Abstraction? :
+# ========================
+
+# Abstraction means hiding the complexity and only showing the essential features of the object.
+# In this way, the user knows what he is doing but not how the work is being done.
+# Any classes in our current software project, that hide internal details provide an abstraction.
+
+# Real World example :
+# =====================
+#
+# Abstraction is a general concept which you can find in the real world as well as in OOP languages.
+# We drive cars with out caring or knowing what is going underneath. The abstraction allows us 
+# to drive a car without knowing the complexity (ie., how the engine transmission, 
+# and braking systems work).
+
+# Abstraction in Python :
+# ==========================
+# I am not sure about it yet!
---
+++ oop/interface.py
@@ -0,0 +1,11 @@
+# Python’s approach to interface design is somewhat different when
+# compared to languages like Java, Go, and C++. These languages all
+# have an interface keyword, while Python does not. Python further
+# deviates from other languages in one other aspect. It doesn’t
+# require the class that’s implementing the interface to define all
+# of the interface’s abstract methods.
+# src : https://realpython.com/python-interface/
+
+# More Details and Discussions :
+# - http://masnun.rocks/2017/04/15/interfaces-in-python-protocols-and-abcs/
+# - https://stackoverflow.com/a/2124415/3158021
