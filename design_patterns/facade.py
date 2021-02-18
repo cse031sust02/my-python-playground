9499f61d04bbb61f0dca3e98770ec94661891705
Talha Ibne Imam <cse031sust02.freelancer@gmail.com>
2021-02-18 15:22:40 +0600

    facade pattern added

--- design_patterns/facade.py
+++ design_patterns/facade.py
@@ -10,8 +10,8 @@ their usage.
 
 Real world example
 ---------------------
-We can turn on a computer by just pressing a button, but internally there are lot
-of stuff to going on. The on/off buttons serve as simple interfaces to all the
+We can turn on a computer by just pressing a button, but internally there are
+lots of stuff going on. The on/off buttons serve as simple interfaces to all the
 underlying procedures.
 
 References
@@ -21,3 +21,43 @@ References
 """
 
 
+class Computer:
+
+    def make_sound(self):
+        print('beeeeeeeeep!')
+
+    def show_loading_screen(self):
+        print('....loading....')
+
+    def load_everything(self):
+        print('loaded everything')
+
+    def show_welcome_message(self):
+        print('welcome user!')
+
+    def close_everything(self):
+        print('closed everything')
+
+    def pull_electricity(self):
+        print('pulled electricty')
+        print('zzzZZZ')
+
+
+class ComputerFacade:
+    def __init__(self):
+        self.computer = Computer()
+
+    def turn_on(self):
+        self.computer.make_sound()
+        self.computer.show_loading_screen()
+        self.computer.load_everything()
+        self.computer.show_welcome_message()
+
+    def turn_off(self):
+        self.computer.close_everything()
+        self.computer.pull_electricity()
+
+
+my_computer = ComputerFacade()
+my_computer.turn_on()
+my_computer.turn_off()
